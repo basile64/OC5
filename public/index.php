dea0ef7c3dc7476ad9deb8bf91e39bdbfd00ae6f
Basile <basile.pineau64@orange.fr>
2024-03-03 23:55:06 +0100

    feat(router): update router and add UrlParser class

--- public/index.php
+++ public/index.php
@@ -6,13 +6,6 @@ error_reporting(E_ALL);
 
 require_once("../config/autoload.php");
 use application\src\controllers as Controller;
-use application\src\models as Model;
 
+$router = new Controller\Router($_GET["url"]);
 
-$router = new Model\router\Router($_GET["url"]);
-
-$router->addRouteGet("/", [new Controller\HomepageController(), "showHomePage"]);
-// $router->addRouteGet("/posts", require_once("../src/controllers/homepageController.php"));
-// $router->addRouteGet("/post/:id", require_once("../src/controllers/postController.php"));
-
-$router->run();
