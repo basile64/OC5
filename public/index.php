d0318dbb11834a3d50a2491752a88cf23d9520ce
Basile <basile.pineau64@orange.fr>
2024-03-20 10:08:29 +0100

    feat(index): implement success message on index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
+if (isset($_SESSION["success_message"])){
+    echo '<div class="success-message">' . $_SESSION['success_message'] . '</div>';
+    unset($_SESSION['success_message']);
+}
+
 require_once("../config/autoload.php");
 use application\src\controllers as Controller;
 
